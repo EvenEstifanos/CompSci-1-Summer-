f6fdf3912b2bb244a249116b72c26883f16476e2
EvenEstifanos <136413472+EvenEstifanos@users.noreply.github.com>
2023-08-06 14:39:47 -0400

    Add files via upload

---
+++ CMSC203_Lab6/CheckingAccount.java
@@ -0,0 +1,22 @@
+
+public class CheckingAccount extends BankAccount
+{
+	public final static double FEE = 0.15 ;
+	
+	public CheckingAccount(String name, double amount)
+	{
+		super(name,amount);
+		setAccountNumber(getAccountNumber() + "-10");
+		
+	}
+	
+	@Override
+	public boolean withdraw (double amount)
+	{
+		
+		return super.withdraw(amount + FEE);
+		
+	}
+	
+	
+}
